ae599f8da9e0f85a8321c795e642371ea0e6b6ad
rania <rania.ahmed2679@gmail.com>
2024-01-09 10:10:29 -0800

    task 0

---
+++ 0x0B-python-input_output/0-read_file.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+""" Defining a read file function"""
+
+
+def read_file(filename=""):
+    """ read utf file """
+    with open(filename, encoding='utf-8') as content
+    print(content.read())
+    
