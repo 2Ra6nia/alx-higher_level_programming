b5e597adf43ccab0fdfef6f33cd4ec8a4ddab1fd
rania <rania.ahmed2679@gmail.com>
2024-01-09 10:31:33 -0800

    task 0

--- 0x0B-python-input_output/0-read_file.py
+++ 0x0B-python-input_output/0-read_file.py
@@ -4,5 +4,5 @@
 
 def read_file(filename=""):
     """ read utf file """
-    with open(filename, encoding='utf-8') as content
+    with open(filename, encoding='utf-8') as content:
     print(content.read(), end="")
