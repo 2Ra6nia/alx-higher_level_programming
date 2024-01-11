9a0dab48ec87401b31235a8e862cebe5dfb6afe7
rania <rania.ahmed2679@gmail.com>
2024-01-10 19:38:11 -0800

    task 2

--- 0x0B-python-input_output/2-append_write.py
+++ 0x0B-python-input_output/2-append_write.py
@@ -5,4 +5,4 @@
 def append_write(filename="", text=""):
     """ append in utf file"""
     with open(filename, "a", encoding="utf-8") as content:
-        content.write(text)
+        return content.write(text)
