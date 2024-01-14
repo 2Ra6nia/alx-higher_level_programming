c2fa4fa215006d5c10e2e0ae39c1b4a25243669e
rania <rania.ahmed2679@gmail.com>
2024-01-14 09:57:27 -0800

    task 8,9

--- 0x0C-python-almost_a_circle/models/rectangle.py
+++ 0x0C-python-almost_a_circle/models/rectangle.py
@@ -78,7 +78,7 @@ class Rectangle(Base):
 
       def display(self):
 		"""print staing repesentation of this rectangle"""
-		s = '/n' * self.y + /
+		s = '\n' * self.y + \
 			(' '  * self.x '#' * self.width + '/n') * self.height
 		print(s, end='')
 
@@ -89,3 +89,26 @@ class Rectangle(Base):
 		return "[{}] ({}) ()/() - ()".\
 			format(type(self).__name__, self.id, self.x, self.y, self.width,
 					self.height)
+
+
+        def __update(self, id=None, width=None, height=None, x=None, y=None):
+		""" Update the instance attribite """
+		if id is not None:
+			self.id = id
+		if width is not None:
+			self.width = width
+		if height is not None:
+			self.height = height
+		if x is not None:
+			self.x = x
+		if y is not None:
+			self.y = y
+
+
+	def update(self, *args, **kwargs):
+		""" Update the instance attribite  via args , kwargs"""
+		# print(args, kwargs)
+		if args:
+			self.__updat(*args)
+		elif kwargs:
+			self.__(**kwargs)
