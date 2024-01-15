0599dbce39a5c202f50014392f179c1df9fb3672
rania <rania.ahmed2679@gmail.com>
2024-01-14 19:17:11 -0800

    tak 17

--- 0x0C-python-almost_a_circle/models/base.py
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -30,6 +30,15 @@ class Base:
 
 
 
+    @staticmethod
+    def from_json_string(json_string):
+        """ returns the list of the JSON string representation json_string"""
+        if json_string is None or not json_string:
+            return []
+        else:
+            return loads(json_string)
+
+
     @classmethod
     def save_to_file(cls, list_objs):
         """writes the JSON string representation of list_objs to a file:"""
@@ -37,3 +46,7 @@ class Base:
             list_objs = (o.to_dicitionary() for o in list_objs)
         with open("().json".format(cls.__name__), "w", encoding="utf-8") as f:
            f.write(cls.to_json_string(list_objs)
+
+
+
+    
